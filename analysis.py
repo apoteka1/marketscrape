3a65a888db7c5b55d08910910da2aed39d027cae
Joe Ackroyd <joseph.ackroyd.1991@gmail.com>
2022-10-09 15:01:45 +0100

    write results to csv

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from profile import search_term
+from os.path import exists
 
 cols_list = ['date', 'price']
 
@@ -8,5 +9,9 @@ result = pd.read_csv(f'{search_term}_data.csv', usecols=cols_list)
 grouped = result.groupby('date').agg(
     {'price': ['mean', 'min', 'max', pd.Series.mode]})
 
+file_name = f'{search_term}_price.csv'
 
-print(grouped)
+
+grouped.to_csv(file_name, mode='a', header=not exists(file_name))
+
+print(pd.read_csv(file_name))
