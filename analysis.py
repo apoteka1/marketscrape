339141bc3c9ddd4aacd23930411d0ab13d323ccd
Joe Ackroyd <joseph.ackroyd.1991@gmail.com>
2022-10-12 09:39:43 +0100

    comments

--- analysis.py
+++ analysis.py
@@ -1,22 +1,25 @@
 import pandas as pd
+import numpy as np
 from profile import search_term
 from os.path import exists
 
-cols_list = ['date', 'price']
+cols_list = ['date', 'price', 'link_url']
 
-result = pd.read_csv(f'{search_term}_data.csv', usecols=cols_list)
+data = pd.read_csv(f'{search_term}_data.csv')
+uniques = data.drop_duplicates(subset=['link_url'])
+print(uniques)
 
 
 def mode(x):
     return pd.Series.mode(x)[0]
 
 
-grouped = result.groupby('date').agg(
-    {'price': ['mean', 'min', 'max', mode]})
+grouped = uniques.groupby('date').agg(
+    {'price': [np.mean, np.min, np.max, mode]})
+
 
 file_name = f'{search_term}_price.csv'
 
-
-grouped.reset_index().to_csv(file_name, mode='a', header=not exists(file_name))
+grouped.to_csv(file_name, mode='a', header=not exists(file_name))
 
 print(pd.read_csv(file_name))
--- scrape.py
+++ scrape.py
@@ -1,3 +1,4 @@
+from os import link
 from profile import password, email, search_term
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,9 +9,9 @@ import re
 import csv
 from datetime import datetime
 
+### selenium ###
 
-today = datetime.now().strftime("%d:%m:%Y")
-
+# block popups, use chrome
 option = Options()
 option.add_argument('--disable-notifications')
 driver = webdriver.Chrome(options=option)
@@ -18,50 +19,72 @@ driver = webdriver.Chrome(options=option)
 driver.get("https://www.facebook.com/")
 
 driver.implicitly_wait(5)
+
+# allow cookies
 accept_cookies = driver.find_element(
     By.XPATH, "//*[@title='Allow essential and optional cookies']")
 accept_cookies.click()
 
+# log in
 email_input = driver.find_element(By.ID, 'email')
 password_input = driver.find_element(By.ID, 'pass')
-
 email_input.send_keys(email)
 password_input.send_keys(password)
-
 driver.find_element(By.NAME, 'login').click()
+
 driver.implicitly_wait(5)
 
+# search for thing
 driver.get(
     f'https://www.facebook.com/marketplace/category/search/?query={search_term}')
 
 driver.implicitly_wait(5)
 
+### Beautiful Soup ###
+
+# get items list block
 item_block = SoupStrainer(
     "div", attrs={"aria-label": "Collection of Marketplace items"})
 
+# make list into soup
 soup = BeautifulSoup(driver.page_source, 'html.parser', parse_only=item_block)
 
-# get exact matches
-titles = soup.find_all('span', text=re.compile(search_term, re.IGNORECASE))
+# get search_term matches, case insensitive (facebook is too relaxed)
+titles = soup.find_all(text=re.compile(search_term, re.IGNORECASE))
 
 results = []
-
+today = datetime.now().strftime("%d:%m:%Y")
+# loop thru matches
 for t in titles:
+
+    # full item block
     parent = t.find_parent("a")
     try:
+        # gets price from block => turns in to number
         price = parent.find(string=re.compile('^£\d+$'))[1:]
+
+        # gets important part of href
+        # removes unique session number / data, so pandas can weed out duplicates using the href
+        item_url_snippet = '/'.join(parent['href'].split('/')[:4])
+
+        # adds dict of data
         i = {}
         i['date'] = today
         i['price'] = int(price)
-        i['link'] = 'https://www.facebook.com'+parent['href']
+        i['link_url'] = 'https://www.facebook.com'+item_url_snippet
         results.append(i)
+
+        # prints if weird prices e.g. commas, letters etc
     except TypeError as e:
-        print(e)
+        print('invalid price format')
+
+### saving data ###
 
 filename = f'{search_term}_data.csv'
 is_new = not exists(filename)
+
 with open(filename, 'a', newline='') as f:
-    w = csv.DictWriter(f, ['date', 'price', 'link'])
+    w = csv.DictWriter(f, ['date', 'price', 'link_url'])
     if is_new:
         w.writeheader()
     for r in results:
